3bfaeb2b757460982f60521db14ba35950492ae6
Phoebe Ng <ng.phoebe8@gmail.com>
2020-02-17 14:07:25 -0800

    fixing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
+import { blue } from "@material-ui/core/colors";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: "#dddddd",
+    backgroundColor: "#ffffff",
     height: "80px",
     borderBottom: "2px solid #00B0F0"
   },
   logo: {
-    height: "50px"
+    height: "55px",
+    margin: "20px",
+    marginRight: "50px"
+  }, 
+  linkBox: {
+    padding: "15px",
+    paddingTop: "31px",
+    paddingBottom: "31px",
+    '&:hover': {
+      backgroundColor: "#eee"
+    },
+    color: "inherit",
+    height: "80px"
+  },
+  signUpButton: {
+    justifyContent: "right",
+    margin: "10px",
+    backgroundColor: "#00B0F0",
+    color: "#ffffff",
+    '&:hover': {
+      backgroundColor: "#73d4f5"
+    }
   }
 }));
 
@@ -22,15 +48,55 @@ export default function Header() {
       className={classes.root}
       style={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-between",
         alignItems: "center"
       }}
     >
-      <img
-        className={classes.logo}
-        src="https://images.squarespace-cdn.com/content/5c464da03917ee46230d8232/1563495118773-4QCOOM46M387IUO76BJK/VforD.jpg?content-type=image%2Fjpeg"
-        alt="vacay logo"
-      />
-    </Box>
+      <Box
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center"
+      }}
+      >
+      <a href="/">
+        <img
+          className={classes.logo}
+          src="https://images.squarespace-cdn.com/content/5c464da03917ee46230d8232/1563495118773-4QCOOM46M387IUO76BJK/VforD.jpg?content-type=image%2Fjpeg"
+          alt="vacay logo"
+        />
+      </a>
+
+        <Typography>
+          <Link href="/" className={classes.linkBox}>About Us</Link>
+          <Link href="/" className={classes.linkBox}>Process</Link>
+          <Link href="/" className={classes.linkBox}>FAQ</Link>
+          <Link href="/" className={classes.linkBox}>Testimonials</Link>
+        </Typography>
+
+      </Box>
+
+      {/* <a href="/">
+        <Box className={classes.linkBox}>
+          <p className={classes.links}>test link</p>
+        </Box>
+      </a>
+
+      <a href="/">
+      <Box className={classes.linkBox}>
+        <p>test link</p>
+      </Box>
+      </a>
+
+       <a href="/">
+      <Box className={classes.linkBox}>
+        <p>test link</p>
+      </Box>
+       </a>
+       </Box> */}
+      <Box>
+        <Button className={classes.signUpButton} variant="contained">Sign Up</Button>
+      </Box>
+    </Box>    
   );
 }
