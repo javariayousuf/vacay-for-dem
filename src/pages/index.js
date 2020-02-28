6670955765ebe09d7b3f0da9d67d9b3abf8dc7d8
javariayousuf <yousujav@uw.edu>
2020-02-28 15:03:52 -0800

    sign up page created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
 export { default as TrainingHome } from "./TrainingHome";
+export { default as Signup } from "./Signup";
