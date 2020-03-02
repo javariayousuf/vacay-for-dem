6cb43b7715b618723f90291788ebb5212a1caa42
mtghuang <mtghuang@uw.edu>
2020-03-01 20:04:44 -0800

    Revert "Revert "add all form fields to page""
    This reverts commit aa65dc9dabcfcc86b39c273d2cc1a1e3a2537331.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
 export { default as TrainingHome } from "./TrainingHome";
 export { default as Signup } from "./Signup";
+export { default as Module } from "./Module";
